cac7594737653b1cd0d3a336bdc2f082ec253384
charafeddine-Web <charafeddinetbibzat@gmail.com>
2024-10-16 09:25:58 +0100

    ajoutré des modification sur css et ajouter des pages,et validation page Home

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
 const nav = document.querySelector("nav");
-
 window.addEventListener ("scroll", function() {
 	nav.classList.toggle ("sticky", window.scrollY > 0);
 });
 
-let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('open');
-};
-
 window.onscroll = () => {
 	menu.classList.remove('bx-x');
 	navbar.classList.remove('open');
